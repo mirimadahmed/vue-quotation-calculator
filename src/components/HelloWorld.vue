c73e3f9a38472c8144e90205bd8e94d55b502d3d
Mir Imad Ahmed <imad.ahmed@gsquad.io>
2020-07-20 00:31:06 +0500

    components created and added to be viewed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,40 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>Checkbox input</h1>
+    <CheckboxInput />
+    <h1>Text input</h1>
+    <TextInput />
+    <h1>Date input</h1>
+    <DateInput />
+    <h1>Radio input</h1>
+    <RadioInput />
+    <h1>Range input</h1>
+    <RangeInput />
+    <h1>Multiselect input</h1>
+    <MultiselectInput />
   </div>
 </template>
 
 <script>
+import CheckboxInput from '@/components/inputs/checkbox-input.vue';
+import DateInput from '@/components/inputs/date-input.vue';
+import MultiselectInput from '@/components/inputs/multiselect-input.vue';
+import RadioInput from '@/components/inputs/radio-input.vue';
+import RangeInput from '@/components/inputs/range-input.vue';
+import TextInput from '@/components/inputs/text-input.vue';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String,
+  },
+  components: {
+    CheckboxInput,
+    DateInput,
+    MultiselectInput,
+    RadioInput,
+    RangeInput,
+    TextInput,
   },
 };
 </script>
---
+++ src/components/inputs/range-input.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>Range Input here</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
