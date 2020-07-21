a1e02e84e9b994d4a0196d4a198fcf3b1c8e9ee7
Mir Imad Ahmed <imad.ahmed@gsquad.io>
2020-07-21 23:58:04 +0500

    checkbox input title and subtitle

--- src/components/inputs/checkbox-input.vue
+++ src/components/inputs/checkbox-input.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="p-3 text-left row">
+    <h2 class="text-left col-12" v-if="settings.title.length > 0">{{ settings.title }}</h2>
+    <p class="text-left col-12" v-if="settings.subTitle.length > 0">{{ settings.subTitle }}</p>
     <div v-for="option in settings.options" :key="option.value" class="form-check form-check-inline col-12 p-3">
       <input
         class="form-check-input"
@@ -40,4 +42,25 @@ export default {
 </script>
 
 <style>
+#holder {
+  height: 50px;
+}
+#label {
+  margin-bottom: -50px;
+  padding-left: 15px;
+  padding-top: 5px;
+  position: absolute;
+  color: #ccc;
+  left: 10px;
+  color: #4ded90;
+}
+#input {
+  font-size: 15px;
+  width: 100%;
+  padding: 23px 25px 15px;
+  border: 1px solid rgba(126, 115, 115, 0.55);
+  margin-top: 0;
+  height: 120%;
+  border-color: #4ded90;
+}
 </style>
