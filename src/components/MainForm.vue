5f20617880b2ecb3905fb0e243e4c637c7db9655
Mir Imad Ahmed <imad.ahmed@gsquad.io>
2020-07-22 10:54:21 +0500

    zip code selection

---
+++ src/components/MainForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import inputs from "@/data/setup.json";
+import CheckboxInput from "@/components/inputs/checkbox-input.vue";
+import DateInput from "@/components/inputs/date-input.vue";
+import MultiselectInput from "@/components/inputs/multiselect-input.vue";
+import RadioInput from "@/components/inputs/radio-input.vue";
+import RangeInput from "@/components/inputs/range-input.vue";
+import TextInput from "@/components/inputs/text-input.vue";
+
+export default {
+  components: {
+    CheckboxInput,
+    DateInput,
+    MultiselectInput,
+    RadioInput,
+    RangeInput,
+    TextInput
+  },
+  computed: {
+    mainSettings() {
+      return inputs;
+    },
+    postCodeMapping() {
+      return inputs.postCodeMapping;
+    },
+    mainServices() {
+      return inputs.services.map(item => item.heading);
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/ZipCodeSelector.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script>
+import TextInput from "@/components/inputs/text-input.vue";
+export default {
+    components: {
+        TextInput
+    },
+}
+</script>
+
+<style>
+
+</style>
